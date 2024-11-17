b2b5de36534eb1f51fb0e5f8db2e413786a3e164
Thodoris Kouleris <kurokokoro@windowslive.com>
2024-11-17 09:13:14 +0200

    blog years

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,7 +8,15 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $blog_data = json_decode(file_get_contents( storage_path()."/data/2024.json"), true);
-        return view('welcome')->with("blog_data", $blog_data);
+        $blog_files = scandir(storage_path().'/data/', SCANDIR_SORT_DESCENDING);
+        $blog_data = [];
+        foreach ($blog_files as $file){
+            if (strlen($file) < 9) continue;
+            $blog_data[str_replace('.json', '', $file)] = json_decode(file_get_contents( storage_path()."/data/$file"), true);
+        }
+        $current_year = date('Y');
+        $current_data = json_decode(file_get_contents( storage_path()."/data/$current_year.json"), true);
+        return view('welcome')->with("current_data", $current_data)
+            ->with("blog_data", $blog_data);
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1161,8 +1161,13 @@
 
                         <div class="row">
                             <div class="col-xs-12 col-sm-12">
+{{--                                <div class="blog-masonry one-column clearfix">--}}
+{{--                                    @foreach($blog_data as $year => $posts)--}}
+{{--                                        <div style="padding-right: 10px;"><a href="#">{{$year}}</a></div>--}}
+{{--                                    @endforeach--}}
+{{--                                </div>--}}
                                 <div class="blog-masonry one-column clearfix">
-                                    @foreach($blog_data as $index => $post)
+                                    @foreach($current_data as $index => $post)
                                         <div class="item post-{{$index}}">
                                             <div class="blog-card">
                                                 <div class="media-block">
