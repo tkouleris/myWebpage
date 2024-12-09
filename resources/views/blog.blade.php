533d748ae1704012c77aacb95af2cc66849b1fb2
Thodoris Kouleris <kurokokoro@windowslive.com>
2024-12-09 06:36:06 +0200

    javascript

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -166,20 +166,6 @@
 
     gtag('config', 'G-CF6G03Q5JN');
 </script>
-<script src="js/jquery.min.js"></script>
-<script src="js/modernizr.custom.js"></script>
-<script src="js/animating.js"></script>
-
-<script src="js/imagesloaded.pkgd.min.js"></script>
-<script src='https://www.google.com/recaptcha/api.js'></script>
-
-<script src='js/perfect-scrollbar.min.js'></script>
-<script src='js/jquery.shuffle.min.js'></script>
-<script src='js/masonry.pkgd.min.js'></script>
-<script src='js/owl.carousel.min.js'></script>
-<script src="js/jquery.magnific-popup.min.js"></script>
-
-<script src="js/validator.js"></script>
-<script src="js/main.js"></script>
+@include('partials.javascript')
 </body>
 </html>
---
+++ resources/views/partials/javascript.blade.php
@@ -0,0 +1,15 @@
+<script src="{{asset('js/jquery.min.js')}}"></script>
+<script src="{{asset('js/modernizr.custom.js')}}"></script>
+<script src="{{asset('js/animating.js')}}"></script>
+
+<script src="{{asset('js/imagesloaded.pkgd.min.js')}}"></script>
+<script src='https://www.google.com/recaptcha/api.js'></script>
+
+<script src='{{asset('js/perfect-scrollbar.min.js')}}'></script>
+<script src='{{asset('js/jquery.shuffle.min.js')}}'></script>
+<script src='{{asset('js/masonry.pkgd.min.js')}}'></script>
+<script src='{{asset('js/masonry.pkgd.min.js')}}'></script>
+<script src="{{asset('js/jquery.magnific-popup.min.js')}}"></script>
+
+<script src="{{asset('js/validator.js')}}"></script>
+<script src="{{asset('js/main.js')}}"></script>
