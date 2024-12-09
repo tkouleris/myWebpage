bd3b162a9334941f3b5eac3345a78841fe618b8b
Thodoris Kouleris <kurokokoro@windowslive.com>
2024-12-09 06:38:12 +0200

    social

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -37,14 +37,7 @@
 
             @include('partials.menu')
 
-            <div class="social-links">
-                <ul>
-                    <li><a href="https://www.linkedin.com/in/thodoris-kouleris-22087460/" target="_blank"><i
-                                class="fab fa-linkedin-in"></i></a></li>
-                    <li><a href="https://blog.tkouleris.eu/" target="_blank"><i class="fab fa-blogger"></i></a></li>
-                    <li><a href="https://twitter.com/tkouleris" target="_blank"><i class="fab fa-twitter"></i></a></li>
-                </ul>
-            </div>
+            @include('partials.social')
 
             @include('partials.cv')
 
