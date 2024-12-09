857814ce1260ea0d14177d5a63f0e2ede5e725c0
Thodoris Kouleris <kurokokoro@windowslive.com>
2024-12-09 06:29:12 +0200

    partials - menu

---
+++ resources/views/partials/menu.blade.php
@@ -0,0 +1,40 @@
+<ul class="main-menu">
+    <li class="active">
+        <a href="{{route('main.index')}}#home" class="nav-anim">
+            <span class="menu-icon lnr lnr-home"></span>
+            <span class="link-text">Home</span>
+        </a>
+    </li>
+    <li>
+        <a href="{{route('main.index')}}#about-me" class="nav-anim">
+            <span class="menu-icon lnr lnr-user"></span>
+            <span class="link-text">About Me</span>
+        </a>
+    </li>
+    <li>
+        <a href="{{route('main.index')}}#resume" class="nav-anim">
+            <span class="menu-icon lnr lnr-graduation-hat"></span>
+            <span class="link-text">Resume</span>
+        </a>
+    </li>
+    <li>
+        <a href="{{route('main.index')}}#portfolio" class="nav-anim">
+            <span class="menu-icon lnr lnr-briefcase"></span>
+            <span class="link-text">Portfolio</span>
+        </a>
+    </li>
+    <li>
+        <a href="{{route('main.index')}}#blog" class="nav-anim">
+            {{--                        <span class="menu-icon lnr lnr-book"></span>--}}
+
+            <span class="menu-icon lnr fab fa-blogger"></span>
+            <span class="link-text">Blog</span>
+        </a>
+    </li>
+    <li>
+        <a href="{{route('main.index')}}#contact" class="nav-anim">
+            <span class="menu-icon lnr lnr-envelope"></span>
+            <span class="link-text">Contact</span>
+        </a>
+    </li>
+</ul>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -35,46 +35,47 @@
         <header id="site_header" class="header mobile-menu-hide">
             @include('partials.header-content')
 
-            <ul class="main-menu">
-                <li class="active">
-                    <a href="#home" class="nav-anim">
-                        <span class="menu-icon lnr lnr-home"></span>
-                        <span class="link-text">Home</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#about-me" class="nav-anim">
-                        <span class="menu-icon lnr lnr-user"></span>
-                        <span class="link-text">About Me</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#resume" class="nav-anim">
-                        <span class="menu-icon lnr lnr-graduation-hat"></span>
-                        <span class="link-text">Resume</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#portfolio" class="nav-anim">
-                        <span class="menu-icon lnr lnr-briefcase"></span>
-                        <span class="link-text">Portfolio</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#blog" class="nav-anim">
-                        {{--                        <span class="menu-icon lnr lnr-book"></span>--}}
+            @include('partials.menu')
+{{--            <ul class="main-menu">--}}
+{{--                <li class="active">--}}
+{{--                    <a href="#home" class="nav-anim">--}}
+{{--                        <span class="menu-icon lnr lnr-home"></span>--}}
+{{--                        <span class="link-text">Home</span>--}}
+{{--                    </a>--}}
+{{--                </li>--}}
+{{--                <li>--}}
+{{--                    <a href="#about-me" class="nav-anim">--}}
+{{--                        <span class="menu-icon lnr lnr-user"></span>--}}
+{{--                        <span class="link-text">About Me</span>--}}
+{{--                    </a>--}}
+{{--                </li>--}}
+{{--                <li>--}}
+{{--                    <a href="#resume" class="nav-anim">--}}
+{{--                        <span class="menu-icon lnr lnr-graduation-hat"></span>--}}
+{{--                        <span class="link-text">Resume</span>--}}
+{{--                    </a>--}}
+{{--                </li>--}}
+{{--                <li>--}}
+{{--                    <a href="#portfolio" class="nav-anim">--}}
+{{--                        <span class="menu-icon lnr lnr-briefcase"></span>--}}
+{{--                        <span class="link-text">Portfolio</span>--}}
+{{--                    </a>--}}
+{{--                </li>--}}
+{{--                <li>--}}
+{{--                    <a href="#blog" class="nav-anim">--}}
+{{--                        --}}{{--                        <span class="menu-icon lnr lnr-book"></span>--}}
 
-                        <span class="menu-icon lnr fab fa-blogger"></span>
-                        <span class="link-text">Blog</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#contact" class="nav-anim">
-                        <span class="menu-icon lnr lnr-envelope"></span>
-                        <span class="link-text">Contact</span>
-                    </a>
-                </li>
-            </ul>
+{{--                        <span class="menu-icon lnr fab fa-blogger"></span>--}}
+{{--                        <span class="link-text">Blog</span>--}}
+{{--                    </a>--}}
+{{--                </li>--}}
+{{--                <li>--}}
+{{--                    <a href="#contact" class="nav-anim">--}}
+{{--                        <span class="menu-icon lnr lnr-envelope"></span>--}}
+{{--                        <span class="link-text">Contact</span>--}}
+{{--                    </a>--}}
+{{--                </li>--}}
+{{--            </ul>--}}
 
             <div class="social-links">
                 <ul>
