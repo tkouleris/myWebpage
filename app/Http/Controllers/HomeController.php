4f6767419bfbc47e46ce6a7359df7918aed2743b
Thodoris Kouleris <kurokokoro@windowslive.com>
2024-12-10 07:05:12 +0200

    fix

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -17,21 +17,17 @@ class HomeController extends Controller
 
     public function blog(Request $request, $page)
     {
-        $blog_files = scandir(storage_path().'/data/', SCANDIR_SORT_DESCENDING);
-        $blog_data = [];
-        foreach ($blog_files as $file){
-            if (strlen($file) < 9) continue;
-            $blog_data[str_replace('.json', '', $file)] = json_decode(file_get_contents( storage_path()."/data/$file"), true);
-        }
+        $year_path = storage_path('data/blog/year');
+        $posts = json_decode(file_get_contents($year_path."/data.json"), true);
         $current_year = date('Y');
-        $current_data = json_decode(file_get_contents( storage_path()."/data/$current_year.json"), true);
-//        $year = $request->input('y');
+        $current_data = $posts[$current_year];
+
+
         $raw_blog_content =  json_decode(file_get_contents( storage_path()."/data/blog/page/data.json"), true);
         $blog_content = $raw_blog_content[$page];
 
 
         return view('blog')->with("current_data", $current_data)
-            ->with("blog_data", $blog_data)
             ->with("post", $blog_content);
     }
 }
