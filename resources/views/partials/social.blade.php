c9d5b59bed49f87541bd680f5e876e09b3f1bca5
Thodoris Kouleris <kurokokoro@windowslive.com>
2024-12-11 06:41:27 +0200

    fix url

--- resources/views/partials/social.blade.php
+++ resources/views/partials/social.blade.php
@@ -1,6 +1,6 @@
 <div class="social-links">
     <ul>
-        <li><a href="https://www.linkedin.com/in/thodoris-kouleris-22087460/" target="_blank"><i
+        <li><a href="https://www.linkedin.com/in/tkouleris/" target="_blank"><i
                     class="fab fa-linkedin-in"></i></a></li>
         <li><a href="https://blog.tkouleris.eu/" target="_blank"><i class="fab fa-blogger"></i></a></li>
         <li><a href="https://twitter.com/tkouleris" target="_blank"><i class="fab fa-twitter"></i></a></li>
