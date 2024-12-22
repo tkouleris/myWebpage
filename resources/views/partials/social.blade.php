e49c0974d0b6da7ef5a6b2567959cb70aaab9ac0
Thodoris Kouleris <kurokokoro@windowslive.com>
2024-12-22 10:11:53 +0200

    update

--- resources/views/partials/social.blade.php
+++ resources/views/partials/social.blade.php
@@ -2,7 +2,6 @@
     <ul>
         <li><a href="https://www.linkedin.com/in/tkouleris/" target="_blank"><i
                     class="fab fa-linkedin-in"></i></a></li>
-        <li><a href="https://blog.tkouleris.eu/" target="_blank"><i class="fab fa-blogger"></i></a></li>
         <li><a href="https://twitter.com/tkouleris" target="_blank"><i class="fab fa-twitter"></i></a></li>
     </ul>
 </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -200,7 +200,7 @@
                                             <div class="ci-text">
                                                 <h4>Where to find me</h4>
                                                 <p>
-                                                    - <a href="https://blog.tkouleris.eu/">My Blog</a><br/>
+                                                    - <a href="https://tkouleris.eu/#blog">My Blog</a><br/>
                                                     - <a href="http://www.facebook.com/kouleris">Facebook</a><br/>
                                                     - <a
                                                         href="https://www.youtube.com/@tkouleris">YouTube</a><br/>
@@ -1125,77 +1125,6 @@
                                             </div>
                                         </div>
                                     @endforeach
-
-
-                                    <!-- Οι πιο συχνοί κωδικοί πρόσβασης -->
-                                    {{--                                    <div class="item post-1">--}}
-                                    {{--                                        <div class="blog-card">--}}
-                                    {{--                                            <div class="media-block">--}}
-                                    {{--                                                <div class="category">--}}
-                                    {{--                                                    <a href="#" title="Misc">Misc</a>--}}
-                                    {{--                                                </div>--}}
-                                    {{--                                                <a target=”_blank” href="https://blog.tkouleris.eu/2024/04/blog-post.html">--}}
-                                    {{--                                                    <img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj6U7dww-meHcqOxR8e9LKvrsLfCgF7-xR5kWq6knwonFUxUtx4B1eoWnGlDcvGZEyUY1561P5eYNTaLeZ-kmhM1YPlZo1eopbFM3yu0pLvSBdk_wd7oN00-vqUhY_vxKsOKKm3rSw60Wo-3tjK-OCaKDBCxxRwdD-25OH2xw6ME_xoNrdPdC6qnUAEZ4RG/w590-h334/imposter.jpg"--}}
-                                    {{--                                                         class="size-blog-masonry-image-two-c"--}}
-                                    {{--                                                         alt="Οι πιο συχνοί κωδικοί πρόσβασης" title="Οι πιο συχνοί κωδικοί πρόσβασης"/>--}}
-                                    {{--                                                    <div class="mask"></div>--}}
-                                    {{--                                                </a>--}}
-                                    {{--                                            </div>--}}
-                                    {{--                                            <div class="post-info">--}}
-                                    {{--                                                <div class="post-date">12 Apr. 2024</div>--}}
-                                    {{--                                                <a target=”_blank” href="https://blog.tkouleris.eu/2024/04/blog-post.html">--}}
-                                    {{--                                                    <h4 class="blog-item-title">Οι πιο συχνοί κωδικοί πρόσβασης</h4>--}}
-                                    {{--                                                </a>--}}
-                                    {{--                                            </div>--}}
-                                    {{--                                        </div>--}}
-                                    {{--                                    </div>--}}
-
-                                    <!-- Ο εθισμός στους υπολογιστές....το 1980 -->
-                                    {{--                                    <div class="item post-1">--}}
-                                    {{--                                        <div class="blog-card">--}}
-                                    {{--                                            <div class="media-block">--}}
-                                    {{--                                                <div class="category">--}}
-                                    {{--                                                    <a href="#" title="Hardware">Misc</a>--}}
-                                    {{--                                                </div>--}}
-                                    {{--                                                <a target=”_blank” href="https://blog.tkouleris.eu/2024/04/1980.html">--}}
-                                    {{--                                                    <img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEi6Q5mnwyuiHFISD11DJyDOaY6G7zB0vSHFY0Avgjv9slKKIXceZUAJLzm2grjBV_Zl-a1Wg3cP3KYZGrGUssFvBSWQv7ohBuYpdAYIUYInFeN-zjbxWVl9GQBWalc1gq4CuHLoU-7-GJsUmQVL0l_JmL3T63WQkf0W_cY-nT-nRAubzshnFTMGJQ6JTfdB/w586-h331/29f8095c-28ac-4d00-8c50-587425dc4b35.png"--}}
-                                    {{--                                                         class="size-blog-masonry-image-two-c"--}}
-                                    {{--                                                         alt="Ο εθισμός στους υπολογιστές....το 1980" title="Ο εθισμός στους υπολογιστές....το 1980"/>--}}
-                                    {{--                                                    <div class="mask"></div>--}}
-                                    {{--                                                </a>--}}
-                                    {{--                                            </div>--}}
-                                    {{--                                            <div class="post-info">--}}
-                                    {{--                                                <div class="post-date">19 Apr. 2024</div>--}}
-                                    {{--                                                <a target=”_blank” href="https://blog.tkouleris.eu/2024/04/1980.html">--}}
-                                    {{--                                                    <h4 class="blog-item-title">Ο εθισμός στους υπολογιστές....το 1980</h4>--}}
-                                    {{--                                                </a>--}}
-                                    {{--                                            </div>--}}
-                                    {{--                                        </div>--}}
-                                    {{--                                    </div>--}}
-
-
-                                    <!-- Πέντε (5) βιβλία που μου άλλαξαν τη ζωή -->
-                                    {{--                                    <div class="item post-1">--}}
-                                    {{--                                        <div class="blog-card">--}}
-                                    {{--                                            <div class="media-block">--}}
-                                    {{--                                                <div class="category">--}}
-                                    {{--                                                    <a href="#" title="Misc">Misc</a>--}}
-                                    {{--                                                </div>--}}
-                                    {{--                                                <a target=”_blank” href="https://blog.tkouleris.eu/2024/04/5.html">--}}
-                                    {{--                                                    <img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj_dX4AkOx0g3yJdL1XXks6blDhEh0a81WwPLrykpciX_j-QxBMKhnKUYkT1XpTMXUHIIDLmvVbMOHd5JL5Ol6uDBRIsUbfcb-mXAo78WrqnhSuARlS7lJnfafV1gyEzOL1dPBdyObuDCQYyPCXAGruKIyUhzvbA5Fu-MMM0sYlne9Mlxl_Bj9wRlQpoqfC/w593-h336/d76a9a29-27cd-4921-9b84-2a5de05ddc97.png"--}}
-                                    {{--                                                         class="size-blog-masonry-image-two-c"--}}
-                                    {{--                                                         alt="Πέντε (5) βιβλία που μου άλλαξαν τη ζωή" title="Πέντε (5) βιβλία που μου άλλαξαν τη ζωή"/>--}}
-                                    {{--                                                    <div class="mask"></div>--}}
-                                    {{--                                                </a>--}}
-                                    {{--                                            </div>--}}
-                                    {{--                                            <div class="post-info">--}}
-                                    {{--                                                <div class="post-date">05 Apr. 2024</div>--}}
-                                    {{--                                                <a target=”_blank” href="https://blog.tkouleris.eu/2024/04/5.html">--}}
-                                    {{--                                                    <h4 class="blog-item-title">Πέντε (5) βιβλία που μου άλλαξαν τη ζωή</h4>--}}
-                                    {{--                                                </a>--}}
-                                    {{--                                            </div>--}}
-                                    {{--                                        </div>--}}
-                                    {{--                                    </div>--}}
 
                                 </div>
                             </div>
