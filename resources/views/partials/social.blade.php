1be2e5152f7362fc5b55fac252c8ab1fe4671ff4
Thodoris Kouleris <kurokokoro@windowslive.com>
2025-01-24 05:40:29 +0200

    facebook social

--- resources/views/partials/social.blade.php
+++ resources/views/partials/social.blade.php
@@ -2,6 +2,6 @@
     <ul>
         <li><a href="https://www.linkedin.com/in/tkouleris/" target="_blank"><i
                     class="fab fa-linkedin-in"></i></a></li>
-        <li><a href="https://twitter.com/tkouleris" target="_blank"><i class="fab fa-twitter"></i></a></li>
+        <li><a href="https://www.facebook.com/kouleris" target="_blank"><i class="fab fa-facebook"></i></a></li>
     </ul>
 </div>
