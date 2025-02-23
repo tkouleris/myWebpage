b67c994a7034df97346e72448586f85f355731a5
Thodoris Kouleris <kurokokoro@windowslive.com>
2025-02-23 09:29:16 +0200

    hide contact form

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1187,60 +1187,60 @@
                                     <h3>How Can I <span>Help You?!</span></h3>
                                 </div>
 
-                                <form id="contact_form" class="contact-form" action="{{ route('mail.send') }}"
-                                      method="post">
-                                    @csrf
+{{--                                <form id="contact_form" class="contact-form" action="{{ route('mail.send') }}"--}}
+{{--                                      method="post">--}}
+{{--                                    @csrf--}}
 
-                                    <div class="messages"></div>
+{{--                                    <div class="messages"></div>--}}
 
-                                    <div class="controls two-columns">
-                                        <div class="fields clearfix">
-                                            <div class="left-column">
-                                                <div class="form-group form-group-with-icon">
-                                                    <input id="form_name" type="text" name="name" class="form-control"
-                                                           placeholder="" required="required"
-                                                           data-error="Name is required.">
-                                                    <label>Full Name</label>
-                                                    <div class="form-control-border"></div>
-                                                    <div class="help-block with-errors"></div>
-                                                </div>
+{{--                                    <div class="controls two-columns">--}}
+{{--                                        <div class="fields clearfix">--}}
+{{--                                            <div class="left-column">--}}
+{{--                                                <div class="form-group form-group-with-icon">--}}
+{{--                                                    <input id="form_name" type="text" name="name" class="form-control"--}}
+{{--                                                           placeholder="" required="required"--}}
+{{--                                                           data-error="Name is required.">--}}
+{{--                                                    <label>Full Name</label>--}}
+{{--                                                    <div class="form-control-border"></div>--}}
+{{--                                                    <div class="help-block with-errors"></div>--}}
+{{--                                                </div>--}}
 
-                                                <div class="form-group form-group-with-icon">
-                                                    <input id="form_email" type="email" name="email"
-                                                           class="form-control" placeholder="" required="required"
-                                                           data-error="Valid email is required.">
-                                                    <label>Email Address</label>
-                                                    <div class="form-control-border"></div>
-                                                    <div class="help-block with-errors"></div>
-                                                </div>
+{{--                                                <div class="form-group form-group-with-icon">--}}
+{{--                                                    <input id="form_email" type="email" name="email"--}}
+{{--                                                           class="form-control" placeholder="" required="required"--}}
+{{--                                                           data-error="Valid email is required.">--}}
+{{--                                                    <label>Email Address</label>--}}
+{{--                                                    <div class="form-control-border"></div>--}}
+{{--                                                    <div class="help-block with-errors"></div>--}}
+{{--                                                </div>--}}
 
-                                                <div class="form-group form-group-with-icon">
-                                                    <input id="form_subject" type="text" name="subject"
-                                                           class="form-control" placeholder="" required="required"
-                                                           data-error="Subject is required.">
-                                                    <label>Subject</label>
-                                                    <div class="form-control-border"></div>
-                                                    <div class="help-block with-errors"></div>
-                                                </div>
-                                            </div>
-                                            <div class="right-column">
-                                                <div class="form-group form-group-with-icon">
-                                                    <textarea id="form_message" name="message" class="form-control"
-                                                              placeholder="" rows="7" required="required"
-                                                              data-error="Please, leave me a message."></textarea>
-                                                    <label>Message</label>
-                                                    <div class="form-control-border"></div>
-                                                    <div class="help-block with-errors"></div>
-                                                </div>
-                                            </div>
-                                        </div>
+{{--                                                <div class="form-group form-group-with-icon">--}}
+{{--                                                    <input id="form_subject" type="text" name="subject"--}}
+{{--                                                           class="form-control" placeholder="" required="required"--}}
+{{--                                                           data-error="Subject is required.">--}}
+{{--                                                    <label>Subject</label>--}}
+{{--                                                    <div class="form-control-border"></div>--}}
+{{--                                                    <div class="help-block with-errors"></div>--}}
+{{--                                                </div>--}}
+{{--                                            </div>--}}
+{{--                                            <div class="right-column">--}}
+{{--                                                <div class="form-group form-group-with-icon">--}}
+{{--                                                    <textarea id="form_message" name="message" class="form-control"--}}
+{{--                                                              placeholder="" rows="7" required="required"--}}
+{{--                                                              data-error="Please, leave me a message."></textarea>--}}
+{{--                                                    <label>Message</label>--}}
+{{--                                                    <div class="form-control-border"></div>--}}
+{{--                                                    <div class="help-block with-errors"></div>--}}
+{{--                                                </div>--}}
+{{--                                            </div>--}}
+{{--                                        </div>--}}
 
-                                        <div class="g-recaptcha" data-sitekey="6LdqmCAUAAAAAMMNEZvn6g4W5e0or2sZmAVpxVqI"
-                                             data-theme="dark"></div>
+{{--                                        <div class="g-recaptcha" data-sitekey="6LdqmCAUAAAAAMMNEZvn6g4W5e0or2sZmAVpxVqI"--}}
+{{--                                             data-theme="dark"></div>--}}
 
-                                        <input type="submit" class="button btn-send" value="Send message">
-                                    </div>
-                                </form>
+{{--                                        <input type="submit" class="button btn-send" value="Send message">--}}
+{{--                                    </div>--}}
+{{--                                </form>--}}
                             </div>
                             <!-- End of Contact Form -->
                         </div>
