568e315d2afbb0c150bb83f33d54cbf31ee964d8
Thodoris Kouleris <kurokokoro@windowslive.com>
2025-12-20 09:23:30 +0200

    fix: main photo caching

--- resources/views/partials/header-content.blade.php
+++ resources/views/partials/header-content.blade.php
@@ -1,6 +1,6 @@
 <div class="header-content">
     <div class="header-photo">
-        <img src="{{asset('img/main_photo.jpg')}}" loading="lazy" alt="Thodoris Kouleris">
+        <img src="{{asset('img/main_photo.jpg')}}?v=1" loading="lazy" alt="Thodoris Kouleris">
     </div>
     <div class="header-titles">
         <h2>Thodoris Kouleris</h2>
