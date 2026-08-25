2373d8a99e7ae81ea13d88c39bf1ed5a5738eb99
Thodoris Kouleris <kurokokoro@windowslive.com>
2026-08-25 21:38:57 +0300

    add Google Analytics script to footer template for tracking

--- resources/views/v3/partials/footer.blade.php
+++ resources/views/v3/partials/footer.blade.php
@@ -1,3 +1,14 @@
 <footer>
     <p>&copy; {{ now()->year }} Thodoris Kouleris</p>
 </footer>
+<script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+
+    gtag('js', new Date());
+
+    gtag('config', 'G-CF6G03Q5JN');
+</script>
